1b06195e486c8bda64bd0353355ad34204ce6b28
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-23 19:59:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ return "NO, NOT SINCE 1938!"
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
